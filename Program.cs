c7cd09f61d82ee5308132e3568852e14d1fd02f7
Josiah Brownlee <buzz72woody@gmail.com>
2023-10-02 10:58:11 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Random_Decimals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double theRandomNumber;
+            int minimumNumber, maximumNumber;
+            var random = new Random();
+            Console.WriteLine("Please tell me the minimum number you want me to generate");
+            minimumNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Great!");
+            Console.WriteLine("Now please tell me the minimum number you want me to generate");
+            maximumNumber = Convert.ToInt32(Console.ReadLine());
+            var rDouble = random.NextDouble() * maximumNumber + minimumNumber;
+            theRandomNumber = random.NextDouble();
+            Console.WriteLine(theRandomNumber);
+
+
+        }
+    }
+}
